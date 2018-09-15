b9097cf7faeefc63ad8321f339ab420a5fe8405a
Ed Paradis <eparadis@users.noreply.github.com>
2018-09-14 20:05:10 -0700

    play notes with your keyboard or the mouse

--- foo.js
+++ foo.js
@@ -1,18 +1,67 @@
 'use strict'
 
 window.onload = () => {
-    const body = document.body;
-    const button = document.createElement('button');
-    body.appendChild(button);
-    button.innerHTML = 'bonk';
-    button.onclick = bonkButton;
+    const body = document.body;   
+    body.onkeypress = keyboardShortcuts;
+    makeKeyboard(body);
 }
 
-function bonkButton() {
-    console.log('bonk!');
-    //create a synth and connect it to the master output (your speakers)
-    var synth = new Tone.Synth().toMaster();
+function makeKeyboard(parent) {
+    [
+        ['z', 'C4'],
+        ['s', 'C#4'],
+        ['x', 'D4'],
+        ['d', 'D#4'],
+        ['c', 'E4'],
+        ['v', 'F4'],
+        ['g', 'F#4'],
+        ['b', 'G4'],
+        ['h', 'G#4'],
+        ['n', 'A4'],
+        ['j', 'A#4'],
+        ['m', 'B4'],
+        [',', 'C5'],
+    ].forEach(k => {
+        parent.appendChild(makeButton(k[0], k[1]));
+    });
+}
 
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
+function keyboardShortcuts(ev) {
+    if( keyboardMap[ev.key]) {
+        keyboardMap[ev.key]();
+        return;
+    }
+    switch( ev.key) {
+        case 'z':
+            bonkButton();
+            break;
+        default:
+            break;
+    }
+}
+
+const keyboardMap = {};
+
+function registerKeyboardShortcut(callback, key) {
+    keyboardMap[key] = callback;
+}
+
+const makeButton = (key, note) => {
+    
+    const element = document.createElement('button');
+    element.className = ['key'];
+    if( note.includes('#')) {
+        element.className += ' blackKey';
+    } else {
+        element.className += ' whiteKey';
+    }
+    element.innerHTML = `${key} - ${note}`;
+    element.onclick = () => {
+        // TODO don't just make these constantly
+        const synth = new Tone.Synth().toMaster();
+        synth.triggerAttackRelease(note, '8n');
+    }
+    registerKeyboardShortcut(element.onclick, key);
+
+    return element;
 }
